670cc176a26f3a8ce672bc4f9337ef26d54b48af
William Liu <william.q.liu@gmail.com>
2016-09-10 13:44:39 -0700

    initial commit

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; // public makes this variable appear in editor
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+//	void Update () // Update is called before rendering a frame (most of our game code)
+//	{
+//		
+//	}
+
+	void FixedUpdate () // FixedUpdate is called before applying any Physics Updates
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
