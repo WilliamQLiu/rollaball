dabf1331b9e94f74636a4e141909eae9c1052638
William Liu <william.q.liu@gmail.com>
2016-09-14 21:36:02 -0700

    Finish with tutorial

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed; // public makes this variable appear in editor
+	public Text countText;  // text for Counter
+	public Text winText;  // text for Winning
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 //	void Update () // Update is called before rendering a frame (most of our game code)
@@ -26,4 +33,23 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter(Collider other)  // other is the Other object being touched
+	{
+		if (other.gameObject.CompareTag ("Pickup")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) { // number of objects
+			winText.text = "You Win!";
+		}
+	}
+		
 }
